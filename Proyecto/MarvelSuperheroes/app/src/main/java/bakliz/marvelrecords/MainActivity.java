36b42a848cc45ded1ab41c06892750dce429455d
AndresBarbosa <andresbob@ciencias.unam.mx>
2018-11-18 17:35:11 -0600

    Ya se consumen datos del sevicio web :D

--- Proyecto/MarvelSuperheroes/app/src/main/java/bakliz/marvelrecords/MainActivity.java
+++ Proyecto/MarvelSuperheroes/app/src/main/java/bakliz/marvelrecords/MainActivity.java
@@ -1,10 +1,13 @@
 package bakliz.marvelrecords;
 
+import android.app.ProgressDialog;
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,17 +16,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
 
     ArrayList<MarvelHero> heroes;
     RecyclerView lvHeroes;
+
+    String llave_publica = "19d9b8c329b60b431fa6ea00a320adf6";
+    String llave_privada = "8edc72e50911111123451ee0901a4611a99385b8";
+
+    ProgressDialog progreso;
+
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+
+    long ts = new Date().getTime();
+
+    String hash =  getMD5("1" + llave_privada + llave_publica);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +57,12 @@ public class MainActivity extends AppCompatActivity implements Response.Listener
         lvHeroes = findViewById(R.id.losHeroes);
         lvHeroes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         heroes = new ArrayList<MarvelHero>();
-        cargarHeroes();
-        AdaptadorRecycler adaptador = new AdaptadorRecycler(heroes);
-        lvHeroes.setAdapter(adaptador);
+
+        request = Volley.newRequestQueue(this);
+
+        cargarWebService();
+
+
         lvHeroes.addOnItemTouchListener(
                 new RecyclerItemClickListener(this, lvHeroes ,new RecyclerItemClickListener.OnItemClickListener() {
                     public void onItemClick(View view, int position) {
@@ -44,6 +72,17 @@ public class MainActivity extends AppCompatActivity implements Response.Listener
                     }
                 })
         );
+    }
+
+    private void cargarWebService() {
+        progreso = new ProgressDialog(this);
+        progreso.setMessage("Consultando...");
+        progreso.show();
+
+        String url = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=19d9b8c329b60b431fa6ea00a320adf6&hash=b12fd8a28a1900f7f5af647fc540f4e5";
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        request.add(jsonObjectRequest);
     }
 
     @Override
@@ -81,13 +120,55 @@ public class MainActivity extends AppCompatActivity implements Response.Listener
                 R.drawable.iron_man));
     }
 
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        Toast.makeText(this, "No se pudo hacer conexion" + " " + error.toString(), Toast.LENGTH_LONG).show();
+        System.out.println();
+        Log.d("ERROR: ", error.toString());
+        progreso.hide();
     }
 
     @Override
     public void onResponse(JSONObject response) {
+        MarvelHero item = null;
+        try {
+            JSONObject lol = response.getJSONObject("data");
+            JSONArray json = lol.optJSONArray("results");
+            for(int i = 0; i < json.length(); i++) {
+                item = new MarvelHero();
+                JSONObject jsonObject = null;
+                jsonObject = json.getJSONObject(i);
 
+                item.setNombre(jsonObject.optString("name"));
+                item.setOrigen(jsonObject.optString("description"));
+                item.setHabilidades(jsonObject.optString("description"));
+                heroes.add(item);
+            }
+            progreso.hide();
+            AdaptadorRecycler adaptador = new AdaptadorRecycler(heroes);
+            lvHeroes.setAdapter(adaptador);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "No hubo conexion con el servidor" + " " + response, Toast.LENGTH_LONG).show();
+            progreso.hide();
+        }
     }
 }
